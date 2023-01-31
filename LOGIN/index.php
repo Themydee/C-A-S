297c2fb59d65f5967f6ad0b90feaa0adc32548e1
Akanbi Bello Temidayo <68824201+Themydee@users.noreply.github.com>
2023-01-31 20:59:06 +0100

    Update index.php

--- LOGIN/index.php
+++ LOGIN/index.php
@@ -1,48 +1,105 @@
+<?php
+    @include 'database/db.php';
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style.css">
-  <title>Document</title>
+  <link rel="shortcut icon" href="CAS Logo Main_PNG.png" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="images/CAS Logo Main_PNG.png" rel="icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <title>CAS VOTING SITE</title>
 </head>
-<body>
-      <div class="login-page">
-        <div class="login-spot">
-          <div class="form-container">
-            <img src="images/CAS Logo Main_PNG.png" width="450px">
-          </div>
-        </div>
 
-        <div class="display">
-          <div class="home-info">
-            <h1 style="color: #fff">VOTING BEGINS IN</h1>
-              <!-- You can change the date time in init.js file -->
-            <ul class="countdown">
-              <li>
-                  <span class="days">14</span>
-                  <h3>Days</h3>
-              </li>
-              <li>
-                  <span class="hours">10</span>
-                  <h3>hours</h3>
-              </li>
-              <li>
-                  <span class="minutes">15</span>
-                  <h3>minutes</h3>
-              </li>
-              <li>
-                  <span class="seconds">34</span>
-                  <h3>seconds</h3>
-              </li>    
-            </ul>
-            <button class="vote-btn" style="width: 200px; margin-bottom: 1rem; " name="vote-btn"><a href="login.php">Vote Now</a></button>
-          </div>
-        </div>
-      </div>
+<body>
+  <nav
+  class="d-flex py-4 bg-white shadow text-right text-dark px-2 px-md-3 px-lg-5"
+>
+<a class="mr-auto" href="#">CAS Vote</a>
   
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-     <script src="js/countdown.js"></script>
-     <script src="js/init.js"></script>
+<a style="text-align: right;" href="login.php">Login</a>
+
+
+
+<!-- <?= $_SESSION['user_name'] ?> -->
+</nav>
+
+
+<div class="bg-white w-100">
+  <div
+    class="cat-head p-5 w-100 text-center"
+    style="
+      background-color: #f1f1f1;
+      font-family: 'Nunito', sans-serif;
+      color: #239540;
+      border-left: solid 15px #0d5817;
+    "
+  >
+    <span class="display-5 text-center"><img src="images/CAS Logo Main_PNG.png" width="70px">  CAS Categories </span>
+  </div>
+  <div class="text-center w-75 mx-auto py-5" style="max-width: 880px">
+  <?php
+    $sql = 'SELECT DISTINCT category FROM categories';
+    $result = mysqli_query($db, $sql);  
+
+    if(mysqli_num_rows($result) > 0) {
+        while($row = mysqli_fetch_assoc($result)) { ?>
+            <a
+            href="<?= 'categories1.php?category=' . $row['category'] ?>"
+            class="m-1 m-md-2 btn cat-link"
+            ><?= $row['category'] ?></a
+          >
+       <?php }
+    }
+  ?>
+
+  </div>
+</div>
+
+<div class="footer"></div>
+<style>
+  
+  .cat-link {
+    border: dashed 1px #999;
+    color: #999;
+  }
+  .cat-link:hover {
+    color: #0cc71b;
+    border-color: #05a10d;
+    box-shadow: none;
+  }
+  .cat-link:active {
+    box-shadow: none;
+  }
+</style>
+
+    <div
+      class="w-100 text-right text-muted mt-n5 p-3 copyright"
+      style="font-size: 0.75rem"
+    >
+      <hr class="d-inline-block w-50 mr-2" style="vertical-align: middle" />
+      Developed by
+      <a target="_blank" href="#" class="my-name"
+        >Coded Farmer</a
+      >
+    </div>
+    
+    <style>
+      .display-5 {
+        font-size: 3rem;
+      }
+      .my-name {
+        border-bottom: 2px dotted;
+        color: #1ab922;
+      }
+      .my-name:hover {
+        color: rgb(12, 95, 70);
+        text-decoration: none;
+      }
+    </style>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/8.11.8/sweetalert2.all.js"></script>
 </body>
 </html>
