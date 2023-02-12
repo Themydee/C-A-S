ddfc408a3d266b0a8322fa9293412e3fea87f597
Akanbi Bello Temidayo <68824201+Themydee@users.noreply.github.com>
2023-02-12 22:59:51 +0100

    Update register.php
    Added PHPMailer effect to differentiate between fake and real emails 
    Using of substr random numbers to send a verification code to users mail

--- LOGIN/register.php
+++ LOGIN/register.php
@@ -1,6 +1,12 @@
 <?php
     @include 'database/db.php';
 
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'vendor/autoload.php';
+
 
 
     if (isset($_POST['submit'])) {
@@ -14,16 +20,69 @@
 
         $result = mysqli_query($db, $select);
 
-        if (mysqli_num_rows($result) > 0) {
-            $error = 'user already exists';
-        }else{
-            if ($pass != $cpass) {
-                $error[] = 'Passwords do not match'; 
-            }else {
-                $insert = "INSERT INTO user_form(name, email, password, user_type) values('$name', '$email', '$pass', '0')";
-                mysqli_query($db, $insert);
-                header('location: login.php');
-            }
+        $mail = new PHPMailer(true);
+
+        try {
+           
+            $mail->SMTPDebug = 2;
+
+            //Send using SMTP
+            $mail->isSMTP();
+
+            //Set the SMTP server to send through
+            $mail->Host = 'smtp.gmail.com';
+
+            //Enable SMTP authentication
+            $mail->SMTPAuth = true;
+
+            //SMTP username
+            $mail->Username = 'nifetemiboy@gmail.com';
+
+            //SMTP password
+            $mail->Password = 'Themydee2018';
+
+            //Enable TLS encryption;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+
+            //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            $mail->Port = 587;
+
+            //Recipients
+            $mail->setFrom('nifetemboy@gmail.com', 'CAS_VOTE');
+
+            //Add a recipient
+            $mail->addAddress($email, $name);
+
+            //Set email format to HTML
+            $mail->isHTML(true);
+
+            $verification_code = substr(number_format(time() * rand(), 0, '', ''), 0, 6);
+
+            $mail->Subject = 'Verify Your Account';
+            $mail->Body    = '<p>Your verification code is: <b style="font-size: 30px;">' . $verification_code . '</b></p>
+            <br><br>
+                    <p>With regrads,</p>
+                    <b>Coded Farmer</b>
+                    https://themydeecodes.000webhostapp.com';
+
+            $mail->send();
+            // echo 'Message has been sent';
+
+            if (mysqli_num_rows($result) > 0) {
+                $error = 'user already exists';
+            }else{
+                if ($pass != $cpass) {
+                    $error[] = 'Passwords do not match'; 
+                }else {
+                    $insert = "INSERT INTO user_form(name, email, password, verification_code, email_verified_at, user_type) values('$name', '$email', '$pass', '$verification_code', '', '0')";
+                    mysqli_query($db, $insert);
+                }
+            }   
+
+            header("Location: verification.php?email=" . $email);
+            exit();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
     };
 
@@ -42,9 +101,9 @@
 <body>
     <div class="login-page">
         <div class="display">
-        <div class="home-info">
-        
-        </div>
+            <div class="home-info">
+            
+            </div>
         </div>
 
         <div class="login-spot">
